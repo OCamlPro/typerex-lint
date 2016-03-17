edaa816e6c7ccc096e84614aa66d1cb0ff11cae7
Théophane Hufschmitt <theophane.hufschmitt@polytechnique.org>
2016-03-17 14:59:32 +0100

    [sempatch] New patch composition based on meta-ppxing
    * Now works the way it should

---
+++ libs/ocp-sempatch/patch_engine.ml
@@ -0,0 +1,21 @@
+open Ast_mapper
+open Ast_helper
+open Asttypes
+open Parsetree
+
+type t = mapper list
+
+let compose patch1 patch2 = patch1 @ patch2
+
+let mkppx patch = {
+  default_mapper with
+  structure = (
+    fun _ structure ->
+      List.fold_left (fun iter elt -> elt.structure elt iter) structure (List.rev patch)
+  );
+
+  signature = (
+    fun _ signature ->
+      List.fold_left (fun iter elt -> elt.signature elt iter) signature (List.rev patch)
+  );
+}
--- libs/ocp-sempatch/ppx_patch.ml
+++ libs/ocp-sempatch/ppx_patch.ml
@@ -52,15 +52,7 @@ let identity_mapper =
   }
 
 (** Mapper composition *)
-let (>>) patch1 patch2 =
-  let (@!) f g x y = (f x) (g identity_mapper y) in (* composition of mapper items -- TODO: Is this really what I wanna do ? *)
-  {
-    patch1 with (* TODO: write the rest *)
-    expr = patch2.expr @! patch1.expr;
-    pat = patch2.pat @! patch1.pat;
-    value_binding = patch2.value_binding @! patch1.value_binding;
-    structure_item = patch2.structure_item @! patch1.structure_item;
-  }
+let (>>) patch1 patch2 = patch2 :: patch1
 
 let txt_is loc = (=) loc.txt
 
@@ -123,27 +115,25 @@ let add_arg_fun fname arg_name =
           default_mapper.value_binding mapper binding
   }
 
-(* TODO: understand what I'm doing and remove the ugly "@guard" annotation *)
 let make_fun_call var_name default_arg = {
   default_mapper with
   expr = fun mapper expr ->
     match expr with
-    | { pexp_desc = Pexp_ident i; pexp_attributes = attrs } when txt_is i (Longident.Lident var_name) && not (List.exists (fun (x, _) -> txt_is x "@guard") attrs) ->
-      let new_attrs = (Location.mkloc "@guard" expr.pexp_loc, PStr []) in
+    | { pexp_desc = Pexp_ident i; pexp_attributes = attrs } when txt_is i (Longident.Lident var_name) ->
       { expr with
-        pexp_desc = Pexp_apply (Exp.ident ~attrs:[new_attrs] (Location.mkloc (Longident.Lident var_name) expr.pexp_loc), [ "", default_arg ]);
+        pexp_desc = Pexp_apply (Exp.ident (Location.mkloc (Longident.Lident var_name) expr.pexp_loc), [ "", default_arg ]);
       }
     | e -> default_mapper.expr mapper e
 }
 
-(** Just discard the argv argument *)
-let to_ppx m = fun _ -> m
+let register name m = Ast_mapper.register name (fun _ -> Patch_engine.mkppx m)
 
 let () =
   let patch =
-    default_mapper
+    []
     >> add_arg_fun "f" "x"
     >> rename_var ">>" ">>!"
-    >> make_fun_call ">>!" (Exp.constant (Const_int 2))
-    >> rename_var "z" "y"
-  in register "patch" (to_ppx patch)
+    >> make_fun_call "f" (Exp.constant (Const_int 2))
+    >> rename_var "x" "y"
+    >> rename_var "y" "z"
+  in register "patch" patch
