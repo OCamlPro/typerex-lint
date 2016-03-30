e9b3eb159f698ccc72c4b9c529d1acf1809a46ae
Théophane Hufschmitt <theophane.hufschmitt@polytechnique.org>
2016-03-30 09:06:29 +0200

    [sempatch] Reworked patch_engine

--- libs/ocp-sempatch/src/patch_engine.ml
+++ libs/ocp-sempatch/src/patch_engine.ml
@@ -7,20 +7,35 @@ let patch_file =
   else
     `Ok Sys.argv.(n-3)
 
+let open_patch () =
+  match patch_file with
+  | `Error _ as e -> e
+  | `Ok f ->
+    try
+      `Ok (open_in f)
+    with
+      Sys_error _ ->
+      `Error "Couldn't open patch file"
+
 let void_formatter =
   Format.make_formatter (fun _ _ _ -> ()) (fun  () -> ())
+
+let ocamlinit =
+  "Topfind.log := fun _ -> ();;" ^
+  "Topfind.add_predicates [ \"byte\"; \"toploop\" ]; Topfind.don't_load [\"findlib\"];;" ^
+  "#require \"compiler-libs\";;" ^
+  "#require \"ppx_patch\";;"
+  (* For testing in local. TODO: Find a way to nicely handle this *)
+  (* ^ "#directory \"lib\";;" *)
+  (* ^ "#load_rec \"ppx_patch.cmo\";;" *)
+
 
 let eval () =
 
   let file_in =
-    match patch_file with
+    match open_patch () with
+    | `Ok f -> f
     | `Error e -> failwith e
-    | `Ok f ->
-      try
-        open_in f
-      with
-        Sys_error _ ->
-        failwith "Couldn't open patch file"
   in
 
   T.initialize_toplevel_env ();
@@ -29,13 +44,9 @@ let eval () =
   Topdirs.dir_directory findlib_dir;
   Topdirs.dir_load Format.err_formatter (findlib_dir ^ Filename.dir_sep ^ "findlib.cma");
   Topdirs.dir_load Format.err_formatter (findlib_dir ^ Filename.dir_sep ^ "findlib_top.cma");
-  T.execute_phrase false void_formatter (!T.parse_toplevel_phrase (Lexing.from_string "Topfind.log := fun _ -> ();;")) |> ignore;
   end;
-  T.execute_phrase false void_formatter (!T.parse_toplevel_phrase (Lexing.from_string "Topfind.add_predicates [ \"byte\"; \"toploop\" ]; Topfind.don't_load [\"findlib\"];;")) |> ignore;
-
-  T.execute_phrase false void_formatter (!T.parse_toplevel_phrase (Lexing.from_string "#require \"compiler-libs\";;")) |> ignore;
-  T.execute_phrase false void_formatter (!T.parse_toplevel_phrase (Lexing.from_string "#require \"ppx_patch\";;")) |> ignore;
-  List.map (T.execute_phrase false Format.std_formatter) (!T.parse_use_file (Lexing.from_channel file_in)) |> ignore;
+  List.map (T.execute_phrase false Format.err_formatter) (!T.parse_use_file (Lexing.from_string ocamlinit)) |> ignore;
+  List.map (T.execute_phrase false Format.err_formatter) (!T.parse_use_file (Lexing.from_channel file_in)) |> ignore;
   close_in file_in
 
 let _ = eval ()
--- libs/ocp-sempatch/test.sh
+++ libs/ocp-sempatch/test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-ocamlbuild -cflag -ppx -cflag "src/patch_engine.byte patch/test_patch.ml" test/test_to_patch.byte
+ocamlbuild -I lib -cflag -ppx -cflag "src/patch_engine.byte patch/test_patch.ml" test/test_to_patch.byte
 ./test_to_patch.byte
