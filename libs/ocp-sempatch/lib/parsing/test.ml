86a6ed69ec52684ce47c081875cd1ca04a9d14ec
Théophane Hufschmitt <theophane.hufschmitt@polytechnique.org>
2016-04-06 12:15:30 +0200

    [sempatch] Added tests

---
+++ libs/ocp-sempatch/lib/parsing/test.ml
@@ -0,0 +1,36 @@
+open Parsetree
+open Std_utils
+
+let test_progs = [
+  "f x y";
+  "x";
+]
+
+let in_file = open_in "test/sempatch.md"
+
+let patches = Patch_parser.sempatch (Patch_lexer.read) (Lexing.from_channel in_file)
+
+let results = [
+  [ true ; true ; true ; false];
+  [ false; true ; true ; false]
+]
+
+let test_asts = List.map (fun s -> Parser.parse_expression Lexer.token (Lexing.from_string s)) test_progs
+
+let apply ast patch =
+  let patch = Ast_pattern_matcher.preprocess patch in
+  match Ast_pattern_matcher.match_expression Parsed_patches.(patch.header.expr_variables) (Ast_pattern_matcher.preprocess_src_expr ast) (Ast_traverser2.Expr Parsed_patches.(patch.body.before)) with
+  | None -> false
+  | Some x -> true
+
+let () =
+  List.iter2
+    (
+      fun ast result ->
+        List.map (apply ast) patches
+        |> List.map2 (=) result
+        |> List.iteri (Printf.printf "test %d : %B\n")
+    )
+    test_asts
+    results;
+  close_in in_file
---
+++ libs/ocp-sempatch/test/sempatch.md
@@ -0,0 +1,20 @@
+# patch0
+```
+f x
+```
+
+# patch1
+```
+x
+```
+
+# patch2
+variables: foo
+```
+foo
+```
+
+# patch3
+```
+foo
+```
