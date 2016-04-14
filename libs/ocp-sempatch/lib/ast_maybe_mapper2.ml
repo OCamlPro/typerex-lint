d3b6b1c8b5f65209555fbba7a13466d8d67a2fb0
Théophane Hufschmitt <rg_ocamlpro@regnat.ovh>
2016-04-14 09:44:29 +0200

    [sempatch] Add parent mappings as arguments to the recursion

--- libs/ocp-sempatch/lib/ast_maybe_mapper2.ml
+++ libs/ocp-sempatch/lib/ast_maybe_mapper2.ml
@@ -2,7 +2,7 @@ open Std_utils
 open Parsetree
 
 type 'a t = {
-  expr : 'a t -> expression -> expression -> (expression * 'a) option
+  expr : 'a t -> 'a -> expression -> expression -> (expression * 'a) option;
 }
 
 let merge_ast_and_env (env_merger : 'a -> 'a -> 'a) =
@@ -14,17 +14,17 @@ let combine mapper merge default elements patch =
   let results = List.map2 mapper elements patch in
   List.fold_left (merge_ast_and_env merge) (Some ([], default)) results
 
-let traverse_expression merge default self e patch =
+let map_expr merge default self defined_vars e patch =
   let maybe_desc =
   match e.pexp_desc, patch.pexp_desc with
   | Pexp_ident _, Pexp_ident _
   | Pexp_constant _, Pexp_constant _ -> None
-  | Pexp_tuple e1s, Pexp_tuple e2s -> Option.map (fun (trees, env) -> (Pexp_tuple trees, env)) @@ combine (self.expr self) merge default e1s e2s
+  | Pexp_tuple e1s, Pexp_tuple e2s -> Option.map (fun (trees, env) -> (Pexp_tuple trees, env)) @@ combine (self.expr self defined_vars) merge default e1s e2s
   | Pexp_apply (f1, [lbl1, arg1]), Pexp_apply (f2, [_lbl2, arg2]) ->
     Option.merge_inf
       (fun (f, env_f) (a, env_a) -> (Pexp_apply (f, [lbl1, a]), merge env_f env_a))
-      (self.expr self f1 f2)
-      (self.expr self arg1 arg2)
+      (self.expr self defined_vars f1 f2)
+      (self.expr self defined_vars arg1 arg2)
   | Pexp_apply _, _ | _, Pexp_apply _
   | Pexp_ident _, _ | _, Pexp_ident _
   | Pexp_constant _, _ | _, Pexp_constant _
@@ -32,10 +32,7 @@ let traverse_expression merge default self e patch =
   | _ -> failwith "Non implemented"
   in Option.map (fun (tree, env) -> { e with pexp_desc = tree; }, env) maybe_desc
 
-let map_expr merge default self e patch =
-  traverse_expression merge default self e patch
-
 let mk merge default = {
-  expr = map_expr merge default
+  expr = map_expr merge default;
 }
 
--- libs/ocp-sempatch/lib/ast_pattern_matcher.ml
+++ libs/ocp-sempatch/lib/ast_pattern_matcher.ml
@@ -3,7 +3,7 @@ open Std_utils
 
 module StringMap = Map.Make(String)
 
-let is_meta : string list -> string -> bool = Fun.flip List.mem
+let empty = StringMap.empty
 
 let apply_replacements tree attributes var_replacements =
   (* TODO : Keep location from the original AST *)
@@ -38,31 +38,31 @@ let apply patch expr =
     let open Ast_maybe_mapper2 in
     let default = mk (StringMap.merge (fun _ -> Misc.const)) StringMap.empty in
     {
-      expr = (fun self ({ pexp_desc = e1; _ } as expr1) ({ pexp_desc = e2; pexp_attributes = attrs2; _ } as expr2) ->
+      expr = (fun self defined_vars ({ pexp_desc = e1; _ } as expr1) ({ pexp_desc = e2; pexp_attributes = attrs2; _ } as expr2) ->
           let replacements =
             match e1, e2 with
             | Pexp_ident i, Pexp_ident j when i.Asttypes.txt = j.Asttypes.txt -> Some (expr1, StringMap.empty)
             | e, Pexp_ident { Asttypes.txt = Longident.Lident j; _ } when is_meta j ->
               Some (expr1, StringMap.singleton j e)
             | _, Pexp_extension (loc, PStr [ { pstr_desc = Pstr_eval (e, _); _ } ]) when loc.Asttypes.txt = "__sempatch_inside" ->
-              apply_to_expr e expr1
-            | _ -> default.expr self expr1 expr2
+              apply_to_expr defined_vars e expr1
+            | _ -> default.expr self defined_vars expr1 expr2
           in
           Option.map (fun (e, env) -> apply_replacements e attrs2 env, env) replacements
         );
     }
-  and apply_to_expr pattern expr =
+  and apply_to_expr defined_vars pattern expr =
     let open Ast_maybe_mapper2 in
     let open Option.Infix in
     let mkapply lbl (expr_f, env_f) (expr_arg, env_arg) = Pexp_apply (expr_f, [lbl, expr_arg]), StringMap.merge (fun _ -> Misc.const) env_f env_arg in
     match expr.pexp_desc with
-    | Pexp_ident _ | Pexp_constant _ -> match_at_root.expr match_at_root expr pattern
+    | Pexp_ident _ | Pexp_constant _ -> match_at_root.expr match_at_root defined_vars expr pattern
     | Pexp_apply (fct, [lbl, arg]) ->
-      let under_f = apply_to_expr pattern fct
-      and under_arg = apply_to_expr pattern arg
-      in let merged = mkapply lbl (under_f |? (fct, StringMap.empty)) (under_arg |? (arg, StringMap.empty));
+      let under_f = apply_to_expr defined_vars pattern fct
+      and under_arg = apply_to_expr defined_vars pattern arg
       in
-      let res = match_at_root.expr match_at_root { expr with pexp_desc = (fst merged) } pattern
+      let merged = mkapply lbl (under_f |? (fct, StringMap.empty)) (under_arg |? (arg, StringMap.empty)) in
+      let res = match_at_root.expr match_at_root defined_vars { expr with pexp_desc = (fst merged) } pattern
       in
       Option.(
         some_if (is_some under_f || is_some under_arg || is_some res)
@@ -72,6 +72,6 @@ let apply patch expr =
           )
       )
     | _ -> failwith "Not implemented yet"
-  in apply_to_expr Parsed_patches.(patch.body) expr
+  in apply_to_expr empty Parsed_patches.(patch.body) expr
      |> Option.map fst
      |> Option.value expr
