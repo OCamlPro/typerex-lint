d5db46d17e714cfe89f203a13a33c6f4e6d1eb17
Théophane Hufschmitt <rg_ocamlpro@regnat.ovh>
2016-04-18 10:02:07 +0200

    [sempatch] [wip] convert the mapper to use Res.t instead of option

--- libs/ocp-sempatch/lib/ast_maybe_mapper2.ml
+++ libs/ocp-sempatch/lib/ast_maybe_mapper2.ml
@@ -1,79 +1,86 @@
 open Std_utils
 open Parsetree
-open Option.Infix
+open Res.Ok_monad_infix
 
 type 'a t = {
-  expr : 'a t -> 'a -> expression -> expression -> (expression * 'a) option;
-  pattern : 'a t -> 'a -> pattern -> pattern -> (pattern * 'a) option;
+  expr : 'a t -> 'a -> expression -> expression -> (expression * 'a, expression * 'a) Error.t;
+  pattern : 'a t -> 'a -> pattern -> pattern -> (pattern * 'a, pattern * 'a) Error.t;
 }
 
-let merge_ast_and_env (env_merger : 'a -> 'a -> 'a) =
-  Option.merge_inf (fun (accu_ast, accu_env) (ast, env) ->
-      (fun new_env -> (ast :: accu_ast, new_env)) (env_merger accu_env env)
-    )
-
-let combine mapper merge default elements patch =
-  let results = List.map2 mapper elements patch in
-  List.fold_left (merge_ast_and_env merge) (Some ([], default)) results
-
-let map_binding merge _default self defined_vars binding patch =
-  self.pattern self defined_vars binding.pvb_pat patch.pvb_pat
-  >>= (fun (pattern, pattern_env) ->
-      self.expr self defined_vars binding.pvb_expr patch.pvb_expr
-      >|= (fun (expr, expr_env) ->
-          { binding with pvb_pat = pattern; pvb_expr = expr; }, merge pattern_env expr_env
-        )
-    )
+(* let merge_ast_and_env (env_merger : 'a -> 'a -> 'a) = *)
+(*   Option.merge_inf (fun (accu_ast, accu_env) (ast, env) -> *)
+(*       (fun new_env -> (ast :: accu_ast, new_env)) (env_merger accu_env env) *)
+(*     ) *)
+(*  *)
+(* let combine mapper merge default elements patch = *)
+(*   let results = List.map2 mapper elements patch in *)
+(*   List.fold_left (merge_ast_and_env merge) (Some ([], default)) results *)
+(*  *)
+(* let map_binding merge _default self defined_vars binding patch = *)
+(*   self.pattern self defined_vars binding.pvb_pat patch.pvb_pat *)
+(*   >>= (fun (pattern, pattern_env) -> *)
+(*       self.expr self defined_vars binding.pvb_expr patch.pvb_expr *)
+(*       >|= (fun (expr, expr_env) -> *)
+(*           { binding with pvb_pat = pattern; pvb_expr = expr; }, merge pattern_env expr_env *)
+(*         ) *)
+(*     ) *)
 
 let map_expr merge default self defined_vars e patch =
   let maybe_desc =
   match e.pexp_desc, patch.pexp_desc with
   | Pexp_ident _, Pexp_ident _
-  | Pexp_constant _, Pexp_constant _ -> None
-  | Pexp_tuple e1s, Pexp_tuple e2s -> Option.map (fun (trees, env) -> (Pexp_tuple trees, env)) @@ combine (self.expr self defined_vars) merge default e1s e2s
-  | Pexp_apply (f1, [lbl1, arg1]), Pexp_apply (f2, [_lbl2, arg2]) ->
-    Option.merge_inf
-      (fun (f, env_f) (a, env_a) -> (Pexp_apply (f, [lbl1, a]), merge env_f env_a))
-      (self.expr self defined_vars f1 f2)
-      (self.expr self defined_vars arg1 arg2)
+  | Pexp_constant _, Pexp_constant _ -> Error (e, default)
+  (* | Pexp_tuple e1s, Pexp_tuple e2s -> Error.map (fun (trees, env) -> (Pexp_tuple trees, env)) @@ combine (self.expr self defined_vars) merge default e1s e2s *)
+  (* | Pexp_apply (f1, [lbl1, arg1]), Pexp_apply (f2, [_lbl2, arg2]) -> *)
+  (*   Option.merge_inf *)
+  (*     (fun (f, env_f) (a, env_a) -> (Pexp_apply (f, [lbl1, a]), merge env_f env_a)) *)
+  (*     (self.expr self defined_vars f1 f2) *)
+  (*     (self.expr self defined_vars arg1 arg2) *)
   | Pexp_fun (lbl1, default1, pat1, expr1), Pexp_fun (_lbl2, _default2, pat2, expr2) ->
     (* TODO: handle labels and default values *)
     let mapped_arg = self.pattern self defined_vars pat1 pat2 in
-    let mapped_expr = mapped_arg >>= (fun (_, env) -> self.expr self env expr1 expr2) in
-    Option.merge_inf
-      (fun (pat, env_pat) (expr, env_expr) -> Pexp_fun (lbl1, default1, pat, expr), merge env_pat env_expr)
-      mapped_arg
-      mapped_expr
-  | Pexp_let (isrecl, bindingsl, exprl), Pexp_let (isrecr, bindingsr, exprr) when isrecl = isrecr ->
-    let mapped_bindings = List.fold_left2 (fun accu bindingl bindingr ->
-        accu
-        >>= (fun (binding_list, env) ->
-            map_binding merge default self defined_vars bindingl bindingr
-            >|= (fun (parsed_binding, new_env) ->
-                parsed_binding :: binding_list, merge env new_env)))
-        (Some ([], defined_vars))
-        bindingsl
-        bindingsr
+    let mapped_expr = mapped_arg
+      >>= (fun (_, env) -> self.expr self env expr1 expr2)
     in
-    mapped_bindings >>= (fun (bindings, env) ->
-        self.expr self env exprl exprr >|= (fun (mapped_expr, env) ->
-            Pexp_let (isrecl, bindings, mapped_expr), env
-          )
-      )
+    begin
+      match mapped_arg, mapped_expr with
+      | Ok (pat, env_pat), Ok (expr, env_expr) -> Ok (Pexp_fun (lbl1, default1, pat, expr), merge env_pat env_expr)
+      | _, _ -> Error (e, defined_vars)
+    end
+  (*   Option.merge_inf *)
+  (*     (fun (pat, env_pat) (expr, env_expr) -> Pexp_fun (lbl1, default1, pat, expr), merge env_pat env_expr) *)
+  (*     mapped_arg *)
+  (*     mapped_expr *)
+  (* | Pexp_let (isrecl, bindingsl, exprl), Pexp_let (isrecr, bindingsr, exprr) when isrecl = isrecr -> *)
+  (*   let mapped_bindings = List.fold_left2 (fun accu bindingl bindingr -> *)
+  (*       accu *)
+  (*       >>= (fun (binding_list, env) -> *)
+  (*           map_binding merge default self defined_vars bindingl bindingr *)
+  (*           >|= (fun (parsed_binding, new_env) -> *)
+  (*               parsed_binding :: binding_list, merge env new_env))) *)
+  (*       (Ok ([], defined_vars)) *)
+  (*       bindingsl *)
+  (*       bindingsr *)
+  (*   in *)
+  (*   mapped_bindings >>= (fun (bindings, env) -> *)
+  (*       self.expr self env exprl exprr >|= (fun (mapped_expr, env) -> *)
+  (*           Pexp_let (isrecl, bindings, mapped_expr), env *)
+  (*         ) *)
+  (*     ) *)
   | Pexp_let _, _ | _, Pexp_let _
   | Pexp_apply _, _ | _, Pexp_apply _
   | Pexp_ident _, _ | _, Pexp_ident _
   | Pexp_constant _, _ | _, Pexp_constant _
-    -> None
+    -> Error (e, default)
   | _ -> failwith "Non implemented"
-  in Option.map (fun (tree, env) -> { e with pexp_desc = tree; }, env) maybe_desc
+  in Error.map (fun (tree, env) -> { e with pexp_desc = tree; }, env) maybe_desc
 
-let map_pattern _merge _default _self _defined_vars p patch =
+let map_pattern _merge default _self _defined_vars p patch =
   let maybe_desc =
     match p.ppat_desc, patch.ppat_desc with
-    | Ppat_var _, _ -> None
+    | Ppat_var _, _ -> Error (p, default)
     |_, _ -> failwith "Non implemented"
-  in Option.map (fun (tree, env) -> { p with ppat_desc = tree; }, env) maybe_desc
+  in Error.map (fun (tree, env) -> { p with ppat_desc = tree; }, env) maybe_desc
 
 let mk merge default = {
   expr = map_expr merge default;
--- libs/ocp-sempatch/lib/ast_pattern_matcher.ml
+++ libs/ocp-sempatch/lib/ast_pattern_matcher.ml
@@ -32,7 +32,8 @@ let apply_replacements tree attributes var_replacements =
 
 let apply patch expr =
   let is_meta_expr e = List.mem e Parsed_patches.(patch.header.meta_expr)
-  and is_meta_binding b = List.mem b Parsed_patches.(patch.header.meta_bindings) in
+  and is_meta_binding b = List.mem b Parsed_patches.(patch.header.meta_bindings)
+  and merge_envs = StringMap.merge (fun _ -> Misc.const) in
   let rec match_at_root =
     let open Ast_maybe_mapper2 in
     let default = mk (StringMap.merge (fun _ -> Misc.const)) StringMap.empty in
@@ -40,96 +41,102 @@ let apply patch expr =
       expr = (fun self defined_vars ({ pexp_desc = e1; _ } as expr1) ({ pexp_desc = e2; pexp_attributes = attrs2; _ } as expr2) ->
           let replacements =
             match e1, e2 with
-            | Pexp_constant c1, Pexp_constant c2 when c1 = c2 -> Some (expr1, defined_vars)
-            | Pexp_ident i, Pexp_ident j when i.Asttypes.txt = j.Asttypes.txt -> Some (expr1, defined_vars)
+            | Pexp_constant c1, Pexp_constant c2 when c1 = c2 -> Ok (expr1, defined_vars)
+            | Pexp_ident i, Pexp_ident j when i.Asttypes.txt = j.Asttypes.txt -> Ok (expr1, defined_vars)
             | Pexp_ident { Asttypes.txt = Longident.Lident i; _ }, Pexp_ident { Asttypes.txt = Longident.Lident j; _ } when is_meta_binding j ->
-              Option.some_if (Variables.get_ident j defined_vars = (Some i)) (expr1, defined_vars)
+              Error.ok_if (Variables.get_ident j defined_vars = (Some i)) (expr1, defined_vars) (expr1, defined_vars)
             | e, Pexp_ident { Asttypes.txt = Longident.Lident j; _ } when is_meta_expr j ->
               (* TODO (one day...) treat the case where j is already defined as an expression *)
-              (* Option.some_if (not (Variables.is_defined_ident j defined_vars)) *)
-              Some (expr1, Variables.add j (Variables.Expression e) empty)
+              Ok (expr1, Variables.add j (Variables.Expression e) empty)
             | _, Pexp_extension (loc, PStr [ { pstr_desc = Pstr_eval (e, _); _ } ]) when loc.Asttypes.txt = "__sempatch_inside" ->
               apply_to_expr defined_vars expr1 e
             | _ -> default.expr self defined_vars expr1 expr2
           in
-          Option.map (fun (e, env) -> apply_replacements e attrs2 env, env) replacements
+          Error.map (fun (e, env) -> apply_replacements e attrs2 env, env) replacements
         );
       pattern = (fun _self _defined_vars pat1 pat2 ->
           let replacements =
             match pat1.ppat_desc, pat2.ppat_desc with
-            | Ppat_var v, Ppat_var v' when v.Asttypes.txt = v'.Asttypes.txt -> Some (pat1, empty)
+            | Ppat_var v, Ppat_var v' when v.Asttypes.txt = v'.Asttypes.txt -> Ok (pat1, empty)
             | Ppat_var { Asttypes.txt = v; _ }, Ppat_var { Asttypes.txt = v'; _ } when is_meta_binding v' ->
-              Some (pat1, StringMap.singleton v' (Variables.Ident v))
-            | _ -> None
+              Ok (pat1, StringMap.singleton v' (Variables.Ident v))
+            | _ -> Error (pat1, empty)
           in replacements
         )
     }
   and apply_to_expr defined_vars expr patch =
     let open Ast_maybe_mapper2 in
-    let open Option.Infix in
-    let merge_two merge_fun x1 default1 x2 default2 =
-      let merged = merge_fun (x1 |? default1) (x2 |? default2) in
-      let res = match_at_root.expr match_at_root defined_vars (fst merged) patch in
-      Option.some_if Option.(is_some x1 || is_some x2 || is_some res) (
-        let res_expr, res_env = res |? merged in
-        res_expr, StringMap.merge (fun _ -> Misc.const) res_env (snd merged)
-      )
-    in
+    let open Res.Err_monad_infix in
 
-    let merge_two_exprs merge_fun e1 e2 =
-      let under_e1 = apply_to_expr defined_vars e1 patch
-      and under_e2 = apply_to_expr defined_vars e2 patch in
-      merge_two
-        (fun x1 x2 -> let (e, env) = merge_fun x1 x2 in { expr with pexp_desc = e }, env)
-        under_e1 (e1, empty)
-        under_e2 (e2, empty)
-    and merge_one_expr merge_fun e =
-      let under = apply_to_expr defined_vars e patch in
-      merge_two (fun x1 _ -> let (e, env) = merge_fun x1 in { expr with pexp_desc = e }, env)
-        under (e, empty)
-        None ()
-    in
-    let mkapply lbl (expr_f, env_f) (expr_arg, env_arg) = Pexp_apply (expr_f, [lbl, expr_arg]), StringMap.merge (fun _ -> Misc.const) env_f env_arg in
+    (* let merge_two merge_fun x1 default1 x2 default2 = *)
+    (*   let merged = merge_fun (x1 |? default1) (x2 |? default2) in *)
+    (*   let res = match_at_root.expr match_at_root defined_vars (fst merged) patch in *)
+    (*   Option.some_if Option.(is_some x1 || is_some x2 || is_some res) ( *)
+    (*     let res_expr, res_env = res |? merged in *)
+    (*     res_expr, StringMap.merge (fun _ -> Misc.const) res_env (snd merged) *)
+    (*   ) *)
+    (* in *)
+    (*  *)
+    (* let merge_two_exprs merge_fun e1 e2 = *)
+    (*   let under_e1 = apply_to_expr defined_vars e1 patch *)
+    (*   and under_e2 = apply_to_expr defined_vars e2 patch in *)
+    (*   merge_two *)
+    (*     (fun x1 x2 -> let (e, env) = merge_fun x1 x2 in { expr with pexp_desc = e }, env) *)
+    (*     under_e1 (e1, empty) *)
+    (*     under_e2 (e2, empty) *)
+    (* and merge_one_expr merge_fun e = *)
+    (*   let under = apply_to_expr defined_vars e patch in *)
+    (*   merge_two (fun x1 _ -> let (e, env) = merge_fun x1 in { expr with pexp_desc = e }, env) *)
+    (*     under (e, empty) *)
+    (*     None () *)
+    (* in *)
+    (* let mkapply lbl (expr_f, env_f) (expr_arg, env_arg) = Pexp_apply (expr_f, [lbl, expr_arg]), StringMap.merge (fun _ -> Misc.const) env_f env_arg in *)
     match expr.pexp_desc with
     | Pexp_ident _ | Pexp_constant _ -> match_at_root.expr match_at_root defined_vars expr patch
-    | Pexp_apply (fct, [lbl, arg]) ->
-      merge_two_exprs (mkapply lbl) fct arg
+    (* | Pexp_apply (fct, [lbl, arg]) -> *)
+    (*   merge_two_exprs (mkapply lbl) fct arg *)
     | Pexp_fun(lbl, default, pat, expr) ->
-      Option.fold
-        (fun _ value -> merge_two_exprs
-            (fun (default_expr, default_env) (body_expr, body_env) ->
-               Pexp_fun (lbl, Some default_expr, pat, body_expr), StringMap.merge (fun _ -> Misc.const) default_env body_env)
-            value expr
+      let apply_some expr = apply_to_expr defined_vars expr patch
+                            |> Res.map (fun (tree, env) -> Some tree, env) in
+      Option.fold (fun _ -> apply_some) (Ok (None, empty)) default
+      >>= (fun (mapped_default, env_default) ->
+          apply_to_expr defined_vars expr patch
+          >>= (fun (mapped_expr, env_expr) ->
+              let self_expr = Ast_helper.Exp.mk (Pexp_fun (lbl, mapped_default, pat, mapped_expr)) in
+              match_at_root.expr match_at_root defined_vars self_expr patch
+              >|= (fun (mapped_self, env_self) ->
+                  mapped_self, merge_envs env_default (merge_envs env_expr env_self)
+                )
+            )
         )
-        (merge_one_expr (fun (expr, env) -> Pexp_fun (lbl, default, pat, expr), env) expr)
-        default
-    | Pexp_let (isrec, bindings, expr) ->
-      let new_bindings_opt = apply_to_bindings defined_vars patch bindings in
-      let under_expr = apply_to_expr (snd (new_bindings_opt |? (bindings, empty))) expr patch in
-      merge_two (fun (bind, bind_env) (expr, expr_env) -> { expr with pexp_desc = Pexp_let (isrec, bind, expr) }, StringMap.merge (fun _ -> Misc.const) bind_env expr_env)
-        new_bindings_opt (bindings, empty)
-        under_expr (expr, empty)
+    (* | Pexp_let (isrec, bindings, expr) -> *)
+    (*   let new_bindings_opt = apply_to_bindings defined_vars patch bindings in *)
+    (*   let under_expr = apply_to_expr (snd (new_bindings_opt |? (bindings, empty))) expr patch in *)
+    (*   merge_two (fun (bind, bind_env) (expr, expr_env) -> { expr with pexp_desc = Pexp_let (isrec, bind, expr) }, StringMap.merge (fun _ -> Misc.const) bind_env expr_env) *)
+    (*     new_bindings_opt (bindings, empty) *)
+    (*     under_expr (expr, empty) *)
     | _ -> failwith "Not implemented yet"
 
-  and apply_to_binding defined_vars patch binding =
-    let open Option.Infix in
-    apply_to_expr defined_vars binding.pvb_expr patch
-    >|= (fun (expr, env) -> { binding with pvb_expr = expr }, env)
-
-  and apply_to_bindings defined_vars patch bindings =
-    let one_true = ref false in
-    let res =List.fold_left
-        (fun (mapped_bindings, accu_env) binding ->
-           let mapped_binding_opt = apply_to_binding defined_vars patch binding in
-           Option.iter (fun _ -> one_true := true) mapped_binding_opt;
-           let (mapped_binding, env) = mapped_binding_opt |? (binding, empty) in
-           (mapped_binding :: mapped_bindings, StringMap.merge (fun _ -> Misc.const) accu_env env)
-        )
-        ([], defined_vars)
-        bindings
-    in Option.some_if !one_true res
+  (* and apply_to_binding defined_vars patch binding = *)
+  (*   let open Error.Err_monad_infix in *)
+  (*   apply_to_expr defined_vars binding.pvb_expr patch *)
+  (*   >|= (fun (expr, env) -> { binding with pvb_expr = expr }, env) *)
+  (*  *)
+  (* and apply_to_bindings defined_vars patch bindings = *)
+  (*   let one_true = ref false in *)
+  (*   let res =List.fold_left *)
+  (*       (fun (mapped_bindings, accu_env) binding -> *)
+  (*          let mapped_binding_opt = apply_to_binding defined_vars patch binding in *)
+  (*          Option.iter (fun _ -> one_true := true) mapped_binding_opt; *)
+  (*          let (mapped_binding, env) = mapped_binding_opt |? (binding, empty) in *)
+  (*          (mapped_binding :: mapped_bindings, StringMap.merge (fun _ -> Misc.const) accu_env env) *)
+  (*       ) *)
+  (*       ([], defined_vars) *)
+  (*       bindings *)
+  (*   in Option.some_if !one_true res *)
 
   in apply_to_expr empty expr Parsed_patches.(patch.body)
-     |> Option.map fst
-     |> Option.value expr
+     |> Error.map fst
+     |> Error.map_err fst
+     |> (function Ok x -> x | Error x -> x)
 
