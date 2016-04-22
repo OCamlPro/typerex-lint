1a78cca885e5f0728c36669f9d4d2156212f2b63
Çağdaş Bozman <cagdas.bozman@ocamlpro.com>
2016-04-22 14:19:57 +0200

    Remove the import of SimpleConfig.
    Now depends on ocplib-system.

--- src/config/configuration.ml
+++ src/config/configuration.ml
@@ -4,6 +4,7 @@ end
 
 module type CONFIG = sig
   val config_file : SimpleConfig.config_file
+  val simple_args : unit -> (string * Arg.spec * string) list
   val create_option :
     string list ->
     ?short_help:string ->
@@ -15,12 +16,16 @@ module type CONFIG = sig
 end
 
 module MakeConfig (C: ConfigArg) = struct
-    let config_file = SimpleConfig.create_config_file C.filename
-    let create_option opt_names ?short_help long_help ?level opt_class default_value =
+  let config_file = SimpleConfig.create_config_file (File.of_string C.filename)
+  let simple_args () = []
+    (* Uncomment this when ocp-build will be updated *)
+    (* SimpleConfig.LowLevel.simple_args "" config_file *)
+
+  let create_option
+      opt_names ?short_help long_help ?level opt_class default_value =
       SimpleConfig.create_option config_file
           opt_names ?short_help long_help ?level
           opt_class default_value
   end
-
 
 module DefaultConfig : CONFIG = MakeConfig(struct let filename = ".ocplint" end)
