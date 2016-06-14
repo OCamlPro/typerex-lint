1815c24030b5749da2d3abf38cc639bf470b1862
Théophane Hufschmitt <theophane.hufschmitt@polytechnique.org>
2016-06-14 17:05:08 +0200

    [sempatch] Restore pretty-printing of ast_elements

--- libs/ocplib-sempatch/lib/ast_element.ml
+++ libs/ocplib-sempatch/lib/ast_element.ml
@@ -4,15 +4,14 @@ open Asttypes
 [%%create_ast_element]
 type t = Element.t
 let to_string =
-  (* let open Pprintast in *)
-  (* let to_string printer value = *)
-  (*   printer Format.str_formatter value; *)
-  (*   Format.flush_str_formatter () *)
-  (* in *)
+  let open Pprintast in
+  let to_string printer value =
+    printer Format.str_formatter value;
+    Format.flush_str_formatter ()
+  in
   function
-  (* | Expression e -> to_string expression e *)
-  (* | String i -> i *)
-  (* | Pattern p -> to_string pattern p *)
+  | Element.Expression e -> to_string expression e
+  | Element.Pattern p -> to_string pattern p
   | _ -> "???"
 
 let from_structure e = Element.Structure e
