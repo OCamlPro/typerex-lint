7a6e4b0cb9b1933a9b12f6b996ba94473fc9541f
Théophane Hufschmitt <theophane.hufschmitt@polytechnique.org>
2016-06-15 11:56:08 +0200

    [sempatch] Only display debug when SEMPATCH_DEBUG is set

--- libs/ocplib-sempatch/lib/std_utils.ml
+++ libs/ocplib-sempatch/lib/std_utils.ml
@@ -276,7 +276,11 @@ sig
 end
 =
 struct
-  let out_fun = ignore
+  let out_fun =
+    try
+      ignore @@ Sys.getenv "SEMPATCH_VERBOSE";
+      print_string
+    with Not_found -> ignore
   let debug msg = Printf.ksprintf
       (fun msg -> out_fun ("Debug : " ^ msg))
       msg
