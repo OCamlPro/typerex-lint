71bece9f5489dafdadda9fe7e790b1b0bc7f5bab
Laporte <michael.laporte@ocamlpro.com>
2016-06-16 10:49:54 +0200

    Improve summary

--- tools/ocp-lint/output/lint_text.ml
+++ tools/ocp-lint/output/lint_text.ml
@@ -25,8 +25,8 @@ let print_warning ppf linter_name warning =
   if warning.loc <> Location.none then
     Format.fprintf ppf "%a" Location.print warning.loc;
 
-  Format.fprintf ppf "  Warning %S number %d:\n"
-    linter_name warning.decl.id;
+  Format.fprintf ppf "  Warning %d %S:\n"
+    warning.decl.id linter_name;
   Format.fprintf ppf "  %s" warning.output;
   Format.fprintf ppf "@."
 
@@ -38,9 +38,11 @@ let check_flag options =
 
 let summary path db =
   let files_linted = ref StringCompat.StringSet.empty in
-  let breakdown = Hashtbl.create 42 in
+  let files_cached = ref StringCompat.StringSet.empty in
+  let breakdown_linted = Hashtbl.create 42 in
+  let breakdown_cached = Hashtbl.create 42 in
   Hashtbl.iter (fun file (hash, pres) ->
-      if Lint_utils.(is_in_path file path) then
+      (* if Lint_utils.(is_in_path file path) then *)
         StringMap.iter (fun pname lres ->
             let flag = check_flag [pname; "enabled"] in
             if flag then
@@ -57,28 +59,58 @@ let summary path db =
                            (files_linted :=
                               StringCompat.StringSet.add file !files_linted;
                             let entry = lname, warning.decl.id in
-                            if Hashtbl.mem breakdown entry then
-                              let old_cpt = Hashtbl.find breakdown entry in
-                              Hashtbl.replace breakdown entry (old_cpt + 1)
-                            else Hashtbl.add breakdown entry 1)
-                      )
+                            if Hashtbl.mem breakdown_linted entry then
+                              let old_cpt = Hashtbl.find breakdown_linted entry in
+                              Hashtbl.replace breakdown_linted entry (old_cpt + 1)
+                            else Hashtbl.add breakdown_linted entry 1))
+                      ws
+                  else
+                  if flag && source = Cache then
+                    let filters =
+                      Lint_globals.Config.get_option_value
+                        [pname; lname; "warnings"] in
+                    let arr = Lint_parse_args.parse_options filters in
+                    List.iter
+                      (fun warning ->
+                         if arr.(warning.decl.id - 1) then
+                           (files_cached :=
+                              StringCompat.StringSet.add file !files_cached;
+                            let entry = lname, warning.decl.id in
+                            if Hashtbl.mem breakdown_cached entry then
+                              let old_cpt = Hashtbl.find breakdown_cached entry in
+                              Hashtbl.replace breakdown_cached entry (old_cpt + 1)
+                            else Hashtbl.add breakdown_cached entry 1))
                       ws)
                 lres)
           pres)
     db;
-  let files = StringCompat.StringSet.cardinal !files_linted in
-  let total = Hashtbl.fold (fun (_, _) cpt acc -> acc + cpt) breakdown 0 in
+  let files_cached_total = StringCompat.StringSet.cardinal !files_cached in
+  let warnings_cached_total =
+    Hashtbl.fold (fun (_, _) cpt acc -> acc + cpt) breakdown_cached 0 in
+  let files_linted_total = StringCompat.StringSet.cardinal !files_linted in
+  let warnings_linted_total =
+    Hashtbl.fold (fun (_, _) cpt acc -> acc + cpt) breakdown_linted 0 in
   Printf.printf "Summary:\n%!";
-  Printf.printf "* %d files were linted\n%!" files;
-  Printf.printf "* %d warnings were emitted:\n%!" total;
+  Printf.printf "== DB Infos ==\n%!";
+  Printf.printf "  root dir: %s\n%!" !(Lint_db.DefaultDB.root);
+  Printf.printf "  * %d file(s) were found in cache:\n%!" files_cached_total;
+  StringCompat.StringSet.iter (Printf.printf "    - %s\n%!") !files_cached;
+  Printf.printf "  * %d warning(s) were found in cache:\n%!" warnings_cached_total;
   Hashtbl.iter (fun (lname, id) cpt ->
-      Printf.printf "  * %d %S number %d\n%!" cpt lname id)
-    breakdown
+      Printf.printf "    - %d %S number %d\n%!" cpt lname id)
+    breakdown_cached;
+  Printf.printf "== Warnings ==\n%!";
+  Printf.printf "  * %d file(s) were linted:\n%!" files_linted_total;
+  StringCompat.StringSet.iter (Printf.printf "    - %s\n%!") !files_linted;
+  Printf.printf "  * %d warning(s) were emitted:\n%!" warnings_linted_total;
+  Hashtbl.iter (fun (lname, id) cpt ->
+      Printf.printf "    - %d %S number %d\n%!" cpt lname id)
+    breakdown_linted
 
 let print fmt path db =
   try
     Hashtbl.iter (fun file (hash, pres) ->
-        if Lint_utils.(is_in_path file path) then
+        (* if Lint_utils.(is_in_path file path) then *)
           StringMap.iter (fun pname lres ->
               let flag = check_flag [pname; "enabled"] in
               if flag then
