d1d4475333a54711ebe5bbc48bb5a2a3af41670b
Laporte <michael.laporte@ocamlpro.com>
2017-03-21 14:35:44 +0100

    Fix plugin_parsing/raw_syntax/avoid_paren.constr_uniq_arg when arg is a typ_arrow

--- plugins/ocp-lint-plugin-parsing/lintParsingLinter.ml
+++ plugins/ocp-lint-plugin-parsing/lintParsingLinter.ml
@@ -158,9 +158,14 @@ module MakeArg = struct
                 | None ->
                   match pcd.pcd_args with
                   (* Detect "A of (int)" *)
-                  | Pcstr_tuple [ { ptyp_desc = Ptyp_tuple [_] }] ->
-                    Warnings.report pcd.pcd_loc
-                      (AvoidParenAroundConstrUniqArg pcd.pcd_name.txt)
+                  | Pcstr_tuple [ { ptyp_desc = Ptyp_tuple [ typ ] }] ->
+                    begin
+                      match typ.ptyp_desc with
+                      | Ptyp_arrow _ -> ()
+                      | _ ->
+                        Warnings.report pcd.pcd_loc
+                          (AvoidParenAroundConstrUniqArg pcd.pcd_name.txt)
+                    end
                   | _ -> ()
                 ) variants
             | _ -> ()
