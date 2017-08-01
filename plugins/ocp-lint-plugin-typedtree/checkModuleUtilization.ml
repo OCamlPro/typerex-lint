20f8cd684b62d76e51b497e018afbc007ffe9143
ClémentDluzniewski <cdluznie@peerocaml.inria.fr>
2017-08-01 16:25:51 +0200

    rewrite the typetree analysis

---
+++ plugins/ocp-lint-plugin-typedtree/checkModuleUtilization.ml
@@ -0,0 +1,141 @@
+let unrecommanded_modules = [
+  Path.Pident (
+      Ident.create "Obj"
+    );
+  Path.Pident (
+      Ident.create "Marshal"
+    );
+  Path.Pident (
+      Ident.create "Printexc"
+    );
+]
+
+let rec same_path p1 p2 =
+  match (p1, p2) with
+  | Path.Pident id1, Path.Pident id2 ->
+     Ident.equal id1 id2
+  | Path.Pdot(p1, s1, _pos1), Path.Pdot(p2, s2, _pos2) ->
+     s1 = s2 && same_path p1 p2
+  | Path.Papply(fun1, arg1), Path.Papply(fun2, arg2) ->
+     same_path fun1 fun2 && same_path arg1 arg2
+  | (_, _) -> false
+
+let path_parent path =
+  let rec aux =
+    function
+    | Path.Pdot (Path.Pident id as p',_,_) -> p'
+    | Path.Pdot (Path.Pdot (_,s,n) as p',_,_) -> Path.Pdot (aux p',s,n)
+    | _ -> failwith "path is not valid"
+  in
+  match path with
+  | Path.Pdot (Path.Pdot _,_,_) | Path.Pdot (Path.Pident _,_,_) ->
+     Some (aux path)
+  | _ ->
+     None
+
+let rec path_child = function
+  | Path.Pident id -> Path.Pident id
+  | Path.Pdot(p, _, _) -> path_child p
+  | _ ->  failwith "path is not valid"
+
+module Linter = Plugin_typedtree.Plugin.MakeLint(struct
+    let name = "Module Utilization"
+    let version = "1"
+    let short_name = "module_utilization"
+    let details = "Checks some properties of module utilizations"
+    let enable = true
+  end)
+
+type warning =
+  | IdentifierInUnrecommandedModule of Path.t * Path.t
+  | UseOpenDirective of Path.t
+
+let w_identifier_in_unrecommanded_mod = Linter.new_warning
+    ~id:1
+    ~short_name:"identifier_from_unrecommanded_module"
+    ~msg:"$ident is in the unrecommanded module $mod."
+    ~severity:1
+
+let w_open_directive = Linter.new_warning
+    ~id:2
+    ~short_name:"use_open_directive"
+    ~msg:"Avoid to use the open directive."
+    ~severity:1
+
+module Warnings = Linter.MakeWarnings(struct
+    type t = warning
+
+    let to_warning = function
+      | IdentifierInUnrecommandedModule (pident, pmod) ->
+	 w_identifier_in_unrecommanded_mod, [
+          ("ident",Path.name pident);
+          ("mod",Path.name pmod)
+        ]
+      | UseOpenDirective p ->
+         w_open_directive, []
+  end)
+
+let iter =
+  let module IterArg = struct
+    include Typedtree_iter.DefaultIteratorArgument
+
+    let is_unrecommanded mpath =
+      List.exists (same_path mpath) unrecommanded_modules
+
+    let process_module_opening mpath loc =
+      Warnings.report loc (UseOpenDirective mpath)
+
+    let process_ident ident_path loc =
+      match path_parent ident_path with
+      | None ->
+         ()
+      | Some parent ->
+         if is_unrecommanded parent then begin
+           Warnings.report
+             loc
+             (IdentifierInUnrecommandedModule (path_child ident_path, parent))
+         end
+
+    let enter_expression expr =
+      let open Typedtree in
+      let open Asttypes in
+      begin match expr.exp_desc with
+      | Texp_ident (ident_path,_,_) -> process_ident ident_path expr.exp_loc
+      | _ -> ()
+      end;
+      List.iter begin function extra,loc,_ ->
+        match extra with
+	| Texp_open (_,p,_,_) -> process_module_opening p loc
+	| _ -> ()
+      end expr.exp_extra
+
+    let enter_pattern pat =
+      let open Typedtree in
+      let open Asttypes in
+      List.iter
+	begin function extra,loc,_ ->
+	   match extra with
+	   | Tpat_open (p,_,_) -> process_module_opening p loc
+	   | _ -> ()
+	end pat.pat_extra
+
+    let enter_structure_item str =
+      let open Typedtree in
+      let open Asttypes in
+      match str.str_desc with
+      | Tstr_open od -> process_module_opening od.open_path od.open_loc
+      | _ -> ()
+
+    let enter_signature_item si =
+      let open Typedtree in
+      let open Asttypes in
+      match si.sig_desc with
+      | Tsig_open od -> process_module_opening od.open_path od.open_loc
+      | _ -> ()
+
+  end in
+  (module IterArg : Typedtree_iter.IteratorArgument)
+
+module MainML = Linter.MakeInputCMT(struct
+    let main cmt = Typedtree_iter.iter_structure iter cmt
+  end)
--- plugins/ocp-lint-plugin-typedtree/checkMutableRecord.ml
+++ plugins/ocp-lint-plugin-typedtree/checkMutableRecord.ml
@@ -2,31 +2,38 @@ module Linter = Plugin_typedtree.Plugin.MakeLint(struct
     let name = "Check Use Of Mutable Field In Record"
     let version = "1"
     let short_name = "check_mutable_record"
-    let details = "details" 
+    let details = "details"
     let enable = true
   end)
 
 type warning =
   | RecordMutableField
-    
+
 let w_mutable_record = Linter.new_warning
     ~id:1
     ~short_name:"mutable record"
     ~msg:"mutable record."
     ~severity:1
-    
+
 module Warnings = Linter.MakeWarnings(struct
     type t = warning
 
     let to_warning = function
       | RecordMutableField ->
 	 w_mutable_record, []
-			    
+
 end)
 
 let iter =
   let module IterArg = struct
     include Typedtree_iter.DefaultIteratorArgument
+
+    let process_record_field_declaration tname label_decl =
+      let open Typedtree in
+      let open Asttypes in
+      if label_decl.ld_mutable = Mutable then begin
+	Warnings.report label_decl.ld_loc (RecordMutableField)
+      end
 
     let enter_structure_item str =
       let open Typedtree in
@@ -34,18 +41,15 @@ let iter =
       match str.str_desc with
       | Tstr_type (_,tps) ->
 	 List.iter begin fun tp ->
-	    match tp.typ_kind with 
+	    match tp.typ_kind with
 	    | Ttype_record lds ->
-	       List.iter begin fun ld ->
-	         if ld.ld_mutable = Mutable then
-		   Warnings.report ld.ld_loc (RecordMutableField);
-	       end lds
+	       List.iter (process_record_field_declaration tp.typ_id) lds
 	    | _ -> ()
 	 end tps
       | _ -> ()
   end in
   (module IterArg : Typedtree_iter.IteratorArgument)
-    
+
 module MainML = Linter.MakeInputCMT(struct
     let main cmt = Typedtree_iter.iter_structure iter cmt
   end)
