14003c022c777dbb7835020d9cd4b8d7921d68ba
Fabrice Le Fessant <fabrice.le_fessant@ocamlpro.com>
2018-04-16 14:49:32 +0200

    Fix OPAM

---
+++ autoconf/travis-ci.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+export OPAMYES=1 OPAMVERBOSE=1
+eval `opam config env`
+
+echo Architecture
+uname -a
+echo OCaml version
+ocaml -version
+echo OPAM versions
+opam --version
+opam --git-version
+
+PREFIX=$HOME/.opam/$OCAML_VERSION
+
+opam pin add my-package .
+opam install my-package
+opam remove my-package
+
