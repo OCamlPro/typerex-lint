18bfa07935affe8b29d8370b7aa4dac007072fdb
Hakimba <hakim.baaloudj@gmail.com>
2018-07-18 11:55:59 +0200

    le checker des arguments finit

--- plugins/ocp-lint-plugin-typedtree/check_order_args.ml
+++ plugins/ocp-lint-plugin-typedtree/check_order_args.ml
@@ -13,7 +13,7 @@ type warning = BadUseArg of (string * string * string)
 let w_badusearg = Linter.new_warning
     ~id:1
     ~short_name:"arg_func_order"
-    ~msg:"the function '$func' with parameters ($good) and their types ($types)"
+    ~msg:"'$func' have arguments with same types, ($good) -> $err_msg"
     ~severity:9
 
 let err_file ?file:(file = "err.txt") str = 
@@ -25,34 +25,30 @@ let rec concat_el l acc = match l with
   | head::tail -> concat_el tail (acc^" "^head)
 
 
-let rec count_same_type l acc = match l with
-  | [] -> acc
-  | hd :: tl -> count_same_type tl (acc + compare_type tl hd 0)
+let rec key_exist l key = match l with
+  [] -> false
+  | (typ,_,_)::tl -> if key = typ then true else key_exist tl key
 
-and compare_type l el acc = match l with
-  | [] -> acc
-  | hd::tl -> if(hd = el) then compare_type tl el (acc+1) else compare_type tl el acc
-
-let rec count_labeled_args l acc = match l with
-  | [] -> acc
-  | hd::tl -> if(hd = "nolabel") then count_labeled_args tl (acc+1) else count_labeled_args tl (acc)
+let rec create_keys l acc = match l with
+  [] -> acc
+  | (label,typ) :: tl -> if(key_exist acc typ) then create_keys tl acc else create_keys tl ([(typ,0,0)] @ acc)
 
 
+let rec cumulative l map = match l with
+  |[] -> map
+  |(label,typ) :: tl -> cumulative tl (List.map (function (t,occur,occur_label) -> if typ = t then 
+                                                                                    if label = "labelled" then 
+                                                                                      (t,occur+1,occur_label+1) 
+                                                                                    else
+                                                                                      (t,occur+1,occur_label)
+                                                                                   else
+                                                                                     (t,occur,occur_label)) map)
 
-let rec if_need_label l acc_type acc_label = match l with
-  |[] -> (acc_type,acc_label)
-  |(label,typ) :: tl -> if_need_label tl (acc_type @ [typ]) (acc_label @ [label])
-
-
-
-let printargs l = match l with
-  | hd::tl -> Printf.printf "%s \n" hd
-  | _ -> Printf.printf "%s \n" ""
 
 module Warnings = Linter.MakeWarnings(struct
     type t = warning
     let to_warning = function
-      | BadUseArg (good_order,func,types) -> w_badusearg, [("good",good_order );("func",func);("types",types)]
+      | BadUseArg (good_order,func,types) -> w_badusearg, [("good",good_order );("func",func);("err_msg",types)]
   end)
 
 let get_string data = match data with
@@ -78,15 +74,17 @@ let get_arg_label arg = let open Asttypes in match arg with
 
 *)
 
-let rec matching_type t types label = let open Typedtree in let open Types in match t.desc  with
-  | Tvar (data) -> Printf.printf "-%s %! \n" label; types @ [(label,get_string data)]
-  | Tarrow (arg_label,t_exp,t__exp,_) -> Printf.printf "-----%s %! \n" label; matching_type t__exp (matching_type t_exp types (get_arg_label arg_label)) (get_arg_label arg_label)
-  | Ttuple (t_list) -> Printf.printf "--%s %! \n" label; get_type_args t_list types label
-  | Tconstr (path,t_list,_) -> Printf.printf "---%s %s %! \n" label (check_path path ""); types @ [(label,check_path path "")]
+let rec matching_type t map_types label = let open Typedtree in let open Types in match t.desc  with
+  | Tvar (data) ->  map_types @ [(label,get_string data)]
+  | Tarrow (arg_label,arg_type,result_type,_) ->   let arg_type = matching_type arg_type [] (get_arg_label arg_label) in
+                                                   let map_types = map_types @ arg_type in matching_type result_type map_types ""
+
+  | Ttuple (t_list) ->  get_type_args t_list map_types label
+  | Tconstr (path,t_list,_) -> map_types @ [(label,check_path path "")]
   | Tobject (_,_) -> [("","tobject")]
   | Tfield (_,_,_,_) -> [("","tfield")]
   | Tnil  -> [("","tnil")]
-  | Tlink (link) -> Printf.printf "----%s %! \n" label; matching_type link types label
+  | Tlink (link) ->  matching_type link map_types label
   | Tsubst (_) -> [("","tsubst")]
   | Tvariant (_) -> [("","tvariant")]
   | Tunivar (_) -> [("","tunivar")]
@@ -98,13 +96,13 @@ let rec matching_type t types label = let open Typedtree in let open Types in ma
 *)
 
 and get_type_args t acc label = let open Typedtree in let open Types in match t with
-  | [] -> acc
-  | hd::tl -> get_type_args tl (matching_type hd acc label) label
+  | [] ->  acc
+  | hd::tl ->  get_type_args tl (matching_type hd acc label) label
 
 
 let get_type_func val_bind = let open Typedtree in match val_bind.vb_expr.exp_type.desc with
   | Tvar (data) -> [("","tvar")]
-  | Tarrow (arg_label,t_exp,t__exp,commut) -> matching_type t_exp (matching_type t__exp [] "") ""
+  | Tarrow (arg_label,arg_type,result_type,commut) -> let arg_type = matching_type arg_type [] (get_arg_label arg_label) in let map_types = arg_type @ [] in matching_type result_type map_types ""
   | Ttuple (t_list) ->  [("","ttuple")]
   | Tconstr (path,t_list,_) -> [("","tconstr")]
   | Tobject (_,_) -> [("","tobject")]
@@ -136,16 +134,19 @@ let rec iter_cases case acc = let open Typedtree in match case.c_lhs.pat_desc wi
 
 let rec sub_list l n acc = match n with
     | 0 -> acc
-    | _ -> sub_list (List.tl l) (n-1) (acc @ [List.hd l])
+    | _ -> sub_list (List.tl l) (n-1) (acc @ [List.hd l]) 
 
 let  get_args_name val_bind = let open Typedtree in match val_bind.vb_expr.exp_desc with
   | Texp_function (record) -> iter_cases (List.hd record.cases) []
   | _ -> []
 
 let get_args_type val_bind = let open Typedtree in match val_bind.vb_expr.exp_desc with
-  | Texp_function (record) -> sub_list (get_type_func val_bind) (List.length (iter_cases(List.hd record.cases) [])) []
+  | Texp_function (record) ->  sub_list (get_type_func val_bind) (List.length (iter_cases(List.hd record.cases) [])) []
   | _ -> []
 
+let rec get_err_msg assoc acc = match assoc with
+  [] -> acc
+  | (typ,occur_t,occur_label)::tl -> if occur_t > occur_label then get_err_msg tl (acc ^""^string_of_int(occur_t)^" "^typ^" found but "^string_of_int(occur_label)^" labelled. ") else get_err_msg tl acc
 
 let iter =
   let module IterExp = struct
@@ -155,24 +156,14 @@ let iter =
       let open Typedtree in 
       let open Asttypes in 
 
-      (*  
-         donc ici, étape par étape
-           1 : je récupère les nom des arguments
-           2 : je récupère la (type,label) list
-           3 : je récupère le nom de la fonction visé
-           4 : je récupère séparemment la liste des types et la liste des label associé, en utilisant "if_need_label" qui va me decouper ma (types,label) list
-           5 : je balance chacune des liste sur leurs fonctions respective qui vont compter le nombre d'arguments non labelisé, et le nombre de fois que deux arguments ont le meme type.
-         
-           exemple : une fonction string -> int -> int -> (string * int * int), le "count_same_type" renvoie 1 car une seule fois on trouve deux arguments du meme type, c'est pas ce que j'veux faire mais en l'état cette fonction agis comme ça.
-           et le "count_labeled_args renvoie 3 car aucun argument n'es labelisé.
-
-           6 : j'appel le warning avec le nom des argument, le nom de la fonction, et le tableau des label qui a été concaténé en une chaine.
-       *)
-
 
       begin match strct.str_desc with
-        | Tstr_value (rec_flag, binding) -> let args_name = get_args_name (List.hd binding) in let args_type = get_args_type (List.hd binding) in 
-          let (txt,loc) = get_func_name (List.hd binding) in let (types,label)  = if_need_label args_type [] [] in let same_type = (count_same_type types 0) in let labeled = (count_labeled_args label 0) in Warnings.report loc (BadUseArg ((concat_el args_name ""),txt, concat_el label ""))
+        | Tstr_value (rec_flag, binding) -> List.iter (function binding -> let args_name = get_args_name binding in let args_type = get_args_type binding in 
+          let (txt,loc) = get_func_name binding in 
+          let assoc_t  = cumulative args_type (create_keys args_type []) in
+          if((get_err_msg assoc_t "") <> "None")
+            then
+              Warnings.report loc (BadUseArg ((concat_el args_name ""),txt,(get_err_msg assoc_t "")))) binding
         | _ -> ()
       end
   end in
--- tests_me/ref/args_same_types.ml
+++ tests_me/ref/args_same_types.ml
@@ -1,1 +1,1 @@
-let func ~a:arg1 arg2 arg3 = (arg2+1,arg1+.1.0,int_of_string(arg3))
+let func arg1 ~n:arg2 ~j:arg3 arg4 arg5 = (int_of_string(arg4),int_of_string(arg5),arg2+1,arg1+.1.0,arg3+8)
